14c397746d10d232a4b50dc32f5d17f4c8638277
eunkong0-0 <joyjessica@naver.com>
2024-07-22 17:53:57 +0900

    Feat : Login 페이지 틀 짜기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Home = () => {
+  // const router = useNavigate();
+
   return (
     <>
-    <Header>logo</Header>
     </>
   )
 }
 
 export default Home
-
-const Header = styled.div`
-  width: 100%;
-  height: 7vh;
-  border-bottom: 1.5px solid #d9d9d9;
-`
