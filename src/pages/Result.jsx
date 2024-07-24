1634b184728a90258c259b2ea61779b97bab553f
eunkong0-0 <joyjessica@naver.com>
2024-07-24 21:17:44 +0900

    돌아가자

---
+++ src/pages/Result.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { ButtonDom, ButtonLink} from './Test'
+import { FiArrowRightCircle, FiShare2 } from 'react-icons/fi'
+import styled from 'styled-components';
+
+const Result = () => {
+
+  return (
+    <>
+    <ResultDom>
+      <div>@9rin_t2 님의 유형은</div>
+      <Image></Image>
+      <h1>귀여운 판다 🐼</h1>
+      <ResultInfo>판다는 귀여우니까 그냥 놀아도 돼용 🩷</ResultInfo>
+      <ButtonDom>
+        <ButtonLink > <FiShare2 /> 테스트 결과 공유하기 </ButtonLink>
+        <ButtonLink style={{ backgroundColor: 'rgba(30, 58, 138, 1)', color:'white' }} to="/"> 
+        <FiArrowRightCircle /> 빙고판 채우러가기 </ButtonLink>
+      </ButtonDom>
+    </ResultDom>
+    </>
+  );
+};
+
+export default Result;
+
+const ResultInfo = styled.div`
+  width: 610px;
+  height: 170px;
+  border: 0px 0px 0px 4px solid rgba(30, 58, 138);
+  color : rgba(30, 58, 138);
+`;
+
+const Image = styled.div`
+  width:200px;
+  height:200px;
+  border: 1px solid black;
+  margin-top : 10px;
+`
+const ResultDom = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding : 100px;
+`;
