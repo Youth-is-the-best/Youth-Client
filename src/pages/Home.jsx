267ad151019c13d320443b1aaba388867fb57e4e
jungsunbeen <tjqls041103@gmail.com>
2024-07-25 23:27:59 +0900

    💄 update Home UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MdOutlineEditCalendar } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -12,12 +13,24 @@ const Home = () => {
       <Header to="/test/0">나의 포트폴리오</Header>
     </Headers>
     <Body>
-      <BingoDom>
+      <LeftDom>
         <h2>열정가득 곰도리의 빙고판</h2>
-      </BingoDom>
-      <InfoDom>
+        <div>2024.01.05 ~ 2024.01.10 <MdOutlineEditCalendar /></div>
+        <BingoDom>
+          <Bingo></Bingo>
+          <Bingo></Bingo>
+          <Bingo></Bingo>
+          <Bingo></Bingo>
+          <Bingo></Bingo>
+          <Bingo></Bingo>
+          <Bingo></Bingo>
+          <Bingo></Bingo>
+          <Bingo></Bingo>
+        </BingoDom>
+      </LeftDom>
+      <RightDom>
         <div>후알유 추천</div>
-      </InfoDom>
+      </RightDom>
     </Body>
     </>
   )
@@ -44,22 +57,47 @@ const Body = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  margin-top : 50px;
+  margin-top : 20px;
 `
-const BingoDom = styled.div`
+const LeftDom = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
+  // align-items: center;
   width : 550px;
   padding : 20px;
 `
-const InfoDom = styled.div`
+
+const RightDom = styled.div`
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  // align-items: center;
+  width : 550px;
+  height : 600px;
+  background: rgba(30, 58, 138, 0.04);
+  border-radius: 0px 20px 20px 0px;
+  border-left : 4px solid rgba(30, 58, 138, 0.4);
+  gap : 15px;
+  padding : 20px;
+`
+
+const BingoDom = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  // gap: 0.5px;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  width : 550px;
+  width : 510px;
   height : 500px;
-  background: rgba(30, 58, 138, 0.04);
-  border-radis : 0px, 20px, 20px, 0px;
-  gap : 15px;
+  padding : 5px;
+`
+
+const Bingo = styled.div`
+  width : 150px;
+  height : 150px;
+  background: rgba(30, 58, 138, 0.1);
+  border-radius : 10px;
 `
