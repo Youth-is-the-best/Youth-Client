a2847bbd305386a44e994ca478f9d37e59160f66
eunkong0-0 <joyjessica@naver.com>
2024-07-26 18:21:43 +0900

    Feat : 이메일 인증 api 연결까지는 했다

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'http://maknaengee.p-e.kr/';
+const baseURL = 'https://maknaengee.p-e.kr';
 
 export const signUp = async (password, username, hash, firstName, lastName, university, college) => {
   const result = await axios.post(`${baseURL}/users/join/`, {
@@ -24,12 +24,46 @@ export const login = async (username, password) => {
   return result.data;
 };
 
-export const duplicate = async () => {
+export const isUsernameDuplicate = async (username) => {
   try {
-    const response = await axios.get(`${baseURL}/users/join/`);
+    const response = await axios.get(`${baseURL}/users/join/`, {
+      params: { username }
+    });
     return response.data.available;
   } catch (error) {
     console.error('username 필드 필요', error);
     throw error;
   }
 };
+
+// export const isReferrerExist = async (referrer) => {
+//   try {
+//     const response = await axios.get(`${baseURL}/users/join/`, {
+//       params: { referrer }
+//     });
+//     return response.data.available;
+//   } catch (error) {
+//     console.error('username 필드 필요', error);
+//     throw error;
+//   }
+// };
+
+export const SendAuthCodeToEmail = async (answer) => {
+  try {
+    const result = await axios.post(`${baseURL}/users/verify/`, answer );
+    return result.data;
+  } catch(error) {
+      console.error(error);
+      throw error;
+  }
+}
+
+export const postAuthCode = async (answer) => {
+  try {
+    const result = await axios.post(`${baseURL}/users/verify/`, answer );
+    return result.data;
+  } catch(error) {
+      console.error(error);
+      throw error;
+  }
+}
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import logo from '../images/logo.png'
 import { useForm } from '../hook/useForm'
-import { signUp, duplicate } from '../apis/user'
+import { signUp, isUsernameDuplicate, SendAuthCodeToEmail, postAuthCode } from '../apis/user'
   
 const Signup = () => {
   const selectList = ["인문계열", "사회계열", "교육계열", "공학계열", "자연계열", "의약계열", "예체능계열"];
@@ -12,24 +12,69 @@ const Signup = () => {
     setCollege(e.target.value);
   };
 
-  const [name, onChangeName] = useForm();
+  const [firstName, onChangeName] = useForm();
   const [email, onChangeEmail] = useForm();
   const [authcode, onChangeAuthcode] = useForm();
-  const [username, onChangeUsername] = useForm();
-  const [password, onChangePassword] = useForm();
   const [confirmPassword, onChangeConfirmPassword] = useForm();
   const [referrer, onChangeReferrer] = useForm();
 
+  const [username, setUsername] = useState('');
+  const [usernameError, setUsernameError] = useState('');
   const [usernameAvailable, setUsernameAvailable] = useState(null);
+
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  // const [referrerAvailable, setReferrerAvailable] = useState(null);
+
   const checkUsername = async () => {
     try {
-      const available = await duplicate(username);
+      const available = await isUsernameDuplicate(username);
       setUsernameAvailable(available);
     } catch(error) {
       setUsernameAvailable(null);
     }
   };
 
+  // const checkReferrer = async () => {
+  //   try {
+  //     const available = await isReferrerExist(referrer);
+  //     setReferrerAvailable(available);
+  //   } catch(error) {
+  //     setReferrerAvailable(null);
+  //   }
+  // };
+
+  const validateUsername = (username) => {
+    const regex = /^[a-zA-Z_]+$/;
+    return regex.test(username);
+  };
+
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    if (validateUsername(username)) {
+      setUsernameError('');
+    } else {
+      setUsernameError('아이디 형식이 올바르지 않습니다.');
+    }
+    setUsername(username);
+  };
+
+  const validatePassword = (password) => {
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(password);
+  };
+
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    if (validatePassword(password)) {
+      setPasswordError('');
+    } else {
+      setPasswordError('비밀번호 형식이 올바르지 않습니다.');
+    }
+    setPassword(password);
+  };
+  
   const isSame = (password, confirmPassword) => {
     return password === confirmPassword;
   };
@@ -37,10 +82,33 @@ const Signup = () => {
   const router = useNavigate();
 
   const onClick = async () => {
-    await signUp(password, username, college);
-    // await signUp(password, username, hash, firstName, lastName, university, college);
+    await signUp(password, username, firstName, college);
+    // await signUp(password, username, hash, firstName, university, college);
     router("/user/login/")
-  }
+  };
+
+  const sendAuthcode = async () => {
+    const answer =
+      {
+        "request_type": "1",
+        "email": email,
+      }
+    console.log(answer);
+    const response = await SendAuthCodeToEmail(answer);
+    console.log("Response:", response);
+  };
+
+  const checkAuthcode = async () => {
+    const answer =
+    {
+      "request_type": "2",
+      "email": email,
+      "verif_code" : authcode,
+    }
+    console.log(answer);
+    const response = await postAuthCode(answer);
+    console.log("Response:", response);
+  };
 
   return (
     <>
@@ -51,13 +119,13 @@ const Signup = () => {
       <Form>
         <Title>회원가입</Title>
         <FormGroup>
-          <Label htmlFor="name">이름*</Label>
-          <input value={name} onChange={onChangeName} id="name" type="text" placeholder="ex. 홍길동" />
+          <Label htmlFor="firstName">이름*</Label>
+          <input value={firstName} onChange={onChangeName} id="firstName" type="text" placeholder="ex. 홍길동" />
         </FormGroup>
         <FormGroup style={{ paddingBottom : 0 }}>
           <Label htmlFor="email">이메일*</Label>
           <input value={email} onChange={onChangeEmail} id="email" type="email" placeholder="학교 이메일 주소를 입력하세요." />
-          <button>인증번호 발송</button>
+          <button onClick={sendAuthcode}>인증번호 발송</button>
         </FormGroup>
         <FormGroup> 
           <Label></Label>
@@ -68,6 +136,7 @@ const Signup = () => {
             type="text" 
             placeholder="인증번호 6자리" />
           <span>4:59</span>
+          <button onClick={checkAuthcode}>인증번호 확인</button>
         </FormGroup>
         <FormGroup>
           <Label htmlFor="school">학교 정보*</Label>
@@ -81,20 +150,39 @@ const Signup = () => {
              ))}
           </select>
         </FormGroup>
-        <FormGroup>
+        <FormGroup style={{ paddingBottom: usernameError && username.length > 0 ? 5 : 15 }}>
           <Label htmlFor="username">아이디(닉네임)*</Label>
-          <input value={username} onChange={onChangeUsername} id="username" type="text" placeholder="영문 대소문자, 특수문자 _ 가능" />
+          <input 
+            value={username} 
+            onChange={onChangeUsername} 
+            id="username" 
+            type="text"
+            placeholder="영문 대소문자, 특수문자 _ 가능"
+            style={{ borderColor: usernameError && username.length ? '#FF0000' : 'initial'}} />
           <button onClick={checkUsername}>중복 확인</button>
         </FormGroup>
         <MessageGroup>
           {
             usernameAvailable === false && <ErrorMessage>이미 사용 중인 아이디입니다.</ErrorMessage>
+          } {
+            usernameError && username.length > 0 && <ErrorMessage>{usernameError}</ErrorMessage>
           }
         </MessageGroup>
-        <FormGroup>
+        <FormGroup style={{ paddingBottom: passwordError && password.length > 0 ? 5 : 15 }}>
           <Label htmlFor="password">비밀번호*</Label>
-          <input value={password} onChange={onChangePassword} id="password" type="password" placeholder="8자리 이상(숫자, 알파벳, 특수문자 1자 이상 포함)" />
+          <input 
+            value={password} 
+            onChange={onChangePassword} 
+            id="password" 
+            type="password" 
+            placeholder="8자리 이상(숫자, 알파벳, 특수문자 1자 이상 포함)"
+            style={{ borderColor: passwordError && password.length ? '#FF0000' : 'initial'}} />
         </FormGroup>
+        <MessageGroup>
+          {
+            passwordError && password.length > 0 && <ErrorMessage>{passwordError}</ErrorMessage>
+          }
+        </MessageGroup>
         <FormGroup style={{ paddingBottom: !isSame(password, confirmPassword) && confirmPassword.length > 0 ? 5 : 15 }} >
           <Label htmlFor="confirmPassword">비밀번호 확인*</Label>
           <input 
@@ -115,9 +203,22 @@ const Signup = () => {
         </MessageGroup>
         <FormGroup>
           <Label htmlFor="referrer">추천인 아이디<br />(선택)</Label>
-          <input value={referrer} onChange={onChangeReferrer} id="referrer" type="text" />
-          <button>아이디 확인</button>
+          <input 
+            value={referrer} 
+            onChange={onChangeReferrer} 
+            id="referrer" 
+            type="text"
+            // style={{ borderColor: referrerAvailable && referrer.length > 0 ? '#1E3A8A' : '#FF0000' }} 
+            />
+          <button onClick={checkUsername}>아이디 확인</button>
         </FormGroup>
+        {/* <MessageGroup>
+          {
+            referrerAvailable === true && <SuccessMessage>추천인 아이디 확인이 완료되었습니다.</SuccessMessage>
+          } {
+            referrerAvailable === false && <ErrorMessage>입력하신 아이디를 찾을 수 없습니다. 다시 입력해주세요.</ErrorMessage>
+          }
+        </MessageGroup> */}
         <FormGroup>
           <Label htmlFor="agreement">이용약관 동의*</Label>
           <AgreeForm>
@@ -273,6 +374,12 @@ const MessageGroup = styled.div`
 
 const ErrorMessage = styled.div`
   color: #FF0000;
+  font-size: 13px;
+  font-weight: 500;
+`;
+
+const SuccessMessage = styled.div`
+  color: #1E3A8A;
   font-size: 13px;
   font-weight: 500;
 `;
